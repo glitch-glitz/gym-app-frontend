d6d532fb443d853a0da62df8ab040acadeceb8be
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-06-02 21:12:22 +0300

    Displayed Members

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,34 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [member, setMember] = useState([]);
 
   //
   useEffect(() => {
     fetch("http://localhost:8000/member")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log((data))
+        setMember(data);
       });
-  }, []) // this only runs once
+  }, []); // this only runs once
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {member.map((member) => (
+        <div key={member.id}>
+          <h3>{member.full_name}</h3>
+          <h4>{member.weight}</h4>
+          <h4>{member.bmi}</h4>
+          <h5>{member.created_at}</h5>
+          <img src={member.passport} alt={member.passport} />
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
